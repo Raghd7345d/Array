72ab676d287ea5cc9918f07e30978d4f179d07b2
Raghd Alabdullah <alabdallahraghad28@gmail.com>
2024-01-26 11:15:21 +0100

    state hinzufügt

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import "./App.css";
+
+function FilterButton() {
+  const [activeFilter, setActiveFilter] = useState("all");
+
+  const handleFilterChange = (filter) => {
+    setActiveFilter(filter);
+  };
+  const isActive = (filter) => {
+    return filter === activeFilter ? "active" : "";
+  };
+  return (
+    <>
+      <div>
+        <h1>Hier ist die Aufgabe</h1>
+
+        <button
+          className={isActive("all")}
+          onClick={() => handleFilterChange("all")}
+        >
+          All
+        </button>
+        <button
+          className={isActive("woman")}
+          onClick={() => handleFilterChange("woman")}
+        >
+          Woman
+        </button>
+        <button
+          className={isActive("man")}
+          onClick={() => handleFilterChange("man")}
+        >
+          Man
+        </button>
+        <button
+          className={isActive("By name")}
+          onClick={() => handleFilterChange("By name")}
+        >
+          By name
+        </button>
+        <button
+          className={isActive("by age")}
+          onClick={() => handleFilterChange("by age")}
+        >
+          By age
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default FilterButton;
